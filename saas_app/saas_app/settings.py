f198d9d7063418cc315822a06a0be728ccdb0062
Sylvia McLaughlin <sylvia.mclaughlin@cds-snc.ca>
2023-02-02 16:52:50 -0800

    Adding files for .devcontainers to work properly.

--- saas_app/saas_app/settings.py
+++ saas_app/saas_app/settings.py
@@ -78,9 +78,14 @@ WSGI_APPLICATION = "saas_app.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'saas',
+        'USER': 'dev',
+        'PASSWORD': "dev",
+        'HOST': "postgresdb",
+        'PORT': '5432',
+        'CONN_MAX_AGE': 60,
     }
 }
 
