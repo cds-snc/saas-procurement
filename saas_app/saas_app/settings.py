ff15a535b18cbd8d813847c6e13cd990bfd9c420
Sylvia McLaughlin <sylvia.mclaughlin@cds-snc.ca>
2023-02-05 08:16:40 -0800

    Making changes so that the .devcontainer is working. Mainly adding environment varialbes in the app container and configuring a vscode user in the docker container

--- saas_app/saas_app/settings.py
+++ saas_app/saas_app/settings.py
@@ -19,7 +19,6 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -130,7 +129,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
