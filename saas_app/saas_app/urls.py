676e2c6bd23e5a63fbed783f682d71bef7f484c9
Sylvia McLaughlin <sylvia.mclaughlin@cds-snc.ca>
2023-02-07 09:38:48 -0800

    Removing unecesary views file and adding extra packages for the requirements_dev file

--- saas_app/saas_app/urls.py
+++ saas_app/saas_app/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 from django.contrib.auth.views import LogoutView
-from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
