ba76478241057776867927e6686c949419d6c317
Guillaume Charest <guillaume.charest@cds-snc.ca>
2023-02-09 13:30:21 -0500

    fix: remove staticfiles path

--- saas_app/saas_app/settings.py
+++ saas_app/saas_app/settings.py
@@ -140,7 +140,6 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
     os.path.join(PROJECT_ROOT, "static"),
 ]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  
