c0a2ca67dc222cf1cc34511edd4f5aace34f09b9
Guillaume Charest <1690085+gcharest@users.noreply.github.com>
2023-02-14 19:12:10 +0000

    remove inexistant PROJECT_ROOT/static path

--- saas_app/saas_app/settings.py
+++ saas_app/saas_app/settings.py
@@ -140,8 +140,8 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    os.path.join(PROJECT_ROOT, "static"),
+    # BASE_DIR / "static",
+    os.path.join(BASE_DIR, "static"),
 ]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  
 
