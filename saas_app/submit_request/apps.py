4ec1862be20e456d45f32b80d353d115eb447e87
Sylvia McLaughlin <85905333+sylviamclaughlin@users.noreply.github.com>
2023-03-01 00:51:10 +0000

    Changes for the form submission feature

---
+++ saas_app/submit_request/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SubmitRequestConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "submit_request"
---
+++ saas_app/submit_request/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+
+from .forms import SubmitRequestForm
+
+
+def process_requets(request):
+    if request.method == 'POST':
+        form = SubmitRequestForm(request.POST)
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # redirect to a new URL:
+            return render(request, 'thanks.html')
+    else:
+        form = SubmitRequestForm()
+    return render(request, 'saas_request.html', {'form': form})
