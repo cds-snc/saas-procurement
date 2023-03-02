61a74d3bfa439d2bd03faab06cc79243a1708ef9
Guillaume Charest <1690085+gcharest@users.noreply.github.com>
2023-03-02 13:53:18 -0500

    Fix/devcontainer db env (#28)
    * fix: add container env for DB
    
    * fix: add install requirements.txt
    
    * fix: fmt

--- saas_app/submit_request/forms.py
+++ saas_app/submit_request/forms.py
@@ -2,24 +2,53 @@ from django import forms
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
 
+
 class SubmitRequestForm(forms.Form):
-    name = forms.CharField(label='What is the name of the Saas?', max_length=100, required=True)
-    url = forms.URLField(label='Please provide the URL', max_length=100, required=True)
-    description = forms.CharField(label='Provide a description and rationale for why you would like to request the SaaS.', widget=forms.Textarea, required=True)
-    cost = forms.CharField(label='What is the monthly or yearly cost (CAD or USD)?', max_length=100, required=True)
-    level_of_subscription = forms.CharField(label='What level of subscription are you requesting?', max_length=100, required=True)
-    number_of_users = forms.IntegerField(label='Please share the number of users', required=True)
-    names_of_users = forms.CharField(label='Please share the names of the users', widget=forms.Textarea, required=True)
-    account_administrator = forms.CharField(label='Who will be the account administrator?', max_length=100, required=True)
-    backup_administrator = forms.CharField(label='Who will be the backup administrator?', max_length=100,required=True)
-    approver = forms.CharField(label='Who is the Product Manager or People Manager that will approve this SaaS request?', max_length=100, required=True)
+    name = forms.CharField(
+        label="What is the name of the Saas?", max_length=100, required=True
+    )
+    url = forms.URLField(label="Please provide the URL", max_length=100, required=True)
+    description = forms.CharField(
+        label="Provide a description and rationale for why you would like to request the SaaS.",
+        widget=forms.Textarea,
+        required=True,
+    )
+    cost = forms.CharField(
+        label="What is the monthly or yearly cost (CAD or USD)?",
+        max_length=100,
+        required=True,
+    )
+    level_of_subscription = forms.CharField(
+        label="What level of subscription are you requesting?",
+        max_length=100,
+        required=True,
+    )
+    number_of_users = forms.IntegerField(
+        label="Please share the number of users", required=True
+    )
+    names_of_users = forms.CharField(
+        label="Please share the names of the users",
+        widget=forms.Textarea,
+        required=True,
+    )
+    account_administrator = forms.CharField(
+        label="Who will be the account administrator?", max_length=100, required=True
+    )
+    backup_administrator = forms.CharField(
+        label="Who will be the backup administrator?", max_length=100, required=True
+    )
+    approver = forms.CharField(
+        label="Who is the Product Manager or People Manager that will approve this SaaS request?",
+        max_length=100,
+        required=True,
+    )
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
-        self.helper.form_id = 'submit_request_form'
-        self.helper.form_class = 'blueForms'
-        self.helper.form_method = 'post'
-        self.helper.form_action = 'submit_saas_form'
+        self.helper.form_id = "submit_request_form"
+        self.helper.form_class = "blueForms"
+        self.helper.form_method = "post"
+        self.helper.form_action = "submit_saas_form"
 
-        self.helper.add_input(Submit('submit', 'Submit'))
+        self.helper.add_input(Submit("submit", "Submit"))
--- saas_app/submit_request/views.py
+++ saas_app/submit_request/views.py
@@ -5,12 +5,12 @@ from .forms import SubmitRequestForm
 
 
 def process_requets(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = SubmitRequestForm(request.POST)
         if form.is_valid():
             # process the data in form.cleaned_data as required
             # redirect to a new URL:
-            return render(request, 'thanks.html')
+            return render(request, "thanks.html")
     else:
         form = SubmitRequestForm()
-    return render(request, 'saas_request.html', {'form': form})
+    return render(request, "saas_request.html", {"form": form})
