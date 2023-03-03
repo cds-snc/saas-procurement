611d3c347492049cec1be875e91206fe46b5f3c3
Sylvia McLaughlin <85905333+sylviamclaughlin@users.noreply.github.com>
2023-03-03 21:53:41 +0000

    Adding files necesary for notify integration

--- saas_app/submit_request/forms.py
+++ saas_app/submit_request/forms.py
@@ -3,6 +3,8 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
 from django.forms import ModelForm
 from .models import SaasRequest
+from saas_app.services import NotifyService
+import os
 
 
 # Submit Saas Request Form
@@ -21,3 +23,8 @@ class SubmitRequestForm(ModelForm):
         self.helper.form_action = "submit_saas_form"
 
         self.helper.add_input(Submit("submit", "Submit"))
+
+        def send_mail(notify_service: NotifyService(os.getenv("NOTIFY_API_KEY"), os.getenv("NOTIFY_API_URL")), email_address, template_id, details):
+            # Send an email to the requestor or approver
+            notify_service.send_email(email_address, template_id, details)
+                                 
--- saas_app/submit_request/views.py
+++ saas_app/submit_request/views.py
@@ -13,6 +13,25 @@ def process_requets(request):
             # get the logged in user (ie the one submitting the request) and save it to the database 
             saas_object.submitted_by = request.user
             saas_object.save()
+
+            # get the requester's email address
+            requestor_email = request.user.email
+            # get the requestors's name
+            requestor_name = request.user.first_name
+            # get the saas_name
+            saas_name = saas_object.name
+            # get thea approver's email address
+            approver_email = saas_object.approver.email
+            # get the approver's name
+            approver_name = saas_name.approver.first_name
+            # get the url
+            url = request.resolver_match.url_name
+
+            # send an email to the requestor
+            form.send_mail(requestor_email, os.getenv("SAAS_SUBMISSION_TEMPLATE_ID"), {"saas_name": saas_name, "name": request.user.first_name, "url": url})
+            # send an email to the approver
+            form.send_mail(approver_email, os.getenv("APPROVAL_REQUEST_TEMPLATE_ID"), {"saas_name": saas_name, "requestor": request.user.first_name, "name": approver_name, "url": url})
+
             # redirect to a new URL:
             return render(request, "thanks.html")
     else:
