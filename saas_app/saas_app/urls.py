9afd749bf28ba41c86901490fa3b091c47e5a08b
Sylvia McLaughlin <85905333+sylviamclaughlin@users.noreply.github.com>
2023-03-09 17:29:50 +0000

    Adding files to display a request, edit and delete it.

--- saas_app/saas_app/urls.py
+++ saas_app/saas_app/urls.py
@@ -21,7 +21,7 @@ from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", TemplateView.as_view(template_name="index.html")),
+    path("", TemplateView.as_view(template_name="index.html"), name="home"),
     path("accounts/", include("allauth.urls")),
     path("logout", LogoutView.as_view()),
     path("submit_request/", include("submit_request.urls")),
--- saas_app/submit_request/forms.py
+++ saas_app/submit_request/forms.py
@@ -1,5 +1,5 @@
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit
+from crispy_forms.layout import Submit, Layout, Button
 from django.forms import ModelForm
 from .models import SaasRequest
 
@@ -29,4 +29,45 @@ class SubmitRequestForm(ModelForm):
         self.helper.form_class = "blueForms"
         self.helper.form_method = "post"
         self.helper.form_action = "submit_saas_form"
-        self.helper.add_input(Submit("submit", "Submit"))
+        self.helper.add_input(Submit("submit", "Submit", css_class="btn btn-primary"))
+        self.helper.add_input(Button('cancel', 'Cancel', css_id="submit", css_class='btn btn-primary btn-lg', onclick="history.back()"))
+
+        
+# Submit Saas Request Form
+class ViewRequestForm(ModelForm):
+    # Form is generated from the database fields.
+    class Meta:
+        model = SaasRequest
+        fields = [
+            "name",
+            "url",
+            "description",
+            "cost",
+            "level_of_subscription",
+            "number_of_users",
+            "names_of_users",
+            "account_administrator",
+            "backup_administrator",
+            "approver",
+        ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_id = "view_request_form"
+        self.helper.form_class = "blueForms"
+        self.helper.layout = Layout(
+            "name",
+            "url",
+            "description",
+            "cost",
+            "level_of_subscription",
+            "number_of_users",
+            "names_of_users",
+            "account_administrator",
+            "backup_administrator",
+            "approver",
+            Submit('save', 'Save changes'),
+            Submit('delete', 'Delete', onclick="return confirm('Are you sure you want to delete this saas request?');"),
+            Button('cancel', 'Cancel', css_id="submit", css_class='btn btn-primary btn-lg', onclick="history.back()")
+        )
