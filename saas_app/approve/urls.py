8f1240476e230934e733308b4bc33ed34561615e
Sylvia McLaughlin <85905333+sylviamclaughlin@users.noreply.github.com>
2023-03-18 00:29:43 +0000

    Adding email messages to the internal ops forms

--- saas_app/approve/urls.py
+++ saas_app/approve/urls.py
@@ -6,5 +6,5 @@ app_name = "approve"
 urlpatterns = [
     path("view/", views.view_all_requests, name="view_all_requests"),
     path("view/<int:pk>/", views.view_request, name="view_request"),
-    path("view/email", views.send_email, name="send_email"),
+    path("send_email/", views.send_email, name="send_email"),
 ]
