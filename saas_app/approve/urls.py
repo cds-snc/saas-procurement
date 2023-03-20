3fbd0e796e9d025a97c1fe235e2a708ba75738ce
Sylvia McLaughlin <85905333+sylviamclaughlin@users.noreply.github.com>
2023-03-20 22:46:47 +0000

    Adding s32 approver url endpoints

--- saas_app/approve/urls.py
+++ saas_app/approve/urls.py
@@ -6,4 +6,14 @@ app_name = "approve"
 urlpatterns = [
     path("view/", views.view_all_requests, name="view_all_requests"),
     path("view/<int:pk>/", views.view_request, name="view_request"),
+     path(
+        "view_s32/",
+        views.view_all_requests_s32_approver,
+        name="view_all_requests_s32_approver",
+    ),
+    path(
+        "view_s32/<int:pk>/",
+        views.view_request_s32_approver,
+        name="view_request_s32_approver",
+    ),
 ]
