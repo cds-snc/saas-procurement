e9633870aa07495e11213655471acbedfb7dc4ae
Sylvia McLaughlin <85905333+sylviamclaughlin@users.noreply.github.com>
2023-03-23 12:43:06 -0700

    Adding a quick healthcheck for the service (#61)
    * Adding a quick healthcheck for the service
    
    * fixing up linting and formatting

--- saas_app/saas_app/settings.py
+++ saas_app/saas_app/settings.py
@@ -71,6 +71,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "saas_app.middleware.HealthCheckMiddleware",
 ]
 
 ROOT_URLCONF = "saas_app.urls"
