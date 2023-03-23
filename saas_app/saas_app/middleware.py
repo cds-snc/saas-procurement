54a71d2caff4cbca621f59c3a061796fcd23958a
Sylvia McLaughlin <85905333+sylviamclaughlin@users.noreply.github.com>
2023-03-23 16:39:46 +0000

    fixing up linting and formatting

--- saas_app/saas_app/middleware.py
+++ saas_app/saas_app/middleware.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponse
 
+
 class HealthCheckMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        if request.path == '/health':
-            return HttpResponse('Health is ok!')
+        if request.path == "/health":
+            return HttpResponse("Health is ok!")
         return self.get_response(request)
