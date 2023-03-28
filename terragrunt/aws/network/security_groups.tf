a68095a1871188382d8f590a8cc13f734dfd3742
Sylvia McLaughlin <sylvia.mclaughlin@cds-snc.ca>
2023-03-27 17:03:56 -0700

    Adding files to create the VPC plus associated security groups

---
+++ terragrunt/aws/network/security_groups.tf
@@ -0,0 +1,85 @@
+# 
+# Security Groups for the VPC Saas Procurement app 
+#
+
+resource "aws_security_group" "app" {
+
+  name        = "${var.product_name}_app_sg"
+  description = "SG for the Saas Procurement App"
+
+  vpc_id = module.saas_procurement_vpc.vpc_id
+
+  tags = {
+    CostCentre = var.billing_code
+    Name       = "${var.product_name}_app_sg"
+  }
+}
+
+resource "aws_security_group" "vpc_endpoint" {
+  name        = "vpc_endpoints"
+  description = "PrivateLink VPC endpoints"
+  vpc_id      = module.saas_procurement_vpc.vpc_id
+
+  tags = {
+    Name       = "${var.product_name}_app_sg"
+    CostCentre = var.billing_code
+    Terraform  = true
+  }
+}
+
+resource "aws_security_group_rule" "app_egress_internet" {
+  description       = "Allow TCP egress connections to the internet on port 443"
+  type              = "egress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "TCP"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.app.id
+}
+
+resource "aws_security_group_rule" "vpc_endpoint_interface_ingress" {
+  description              = "Ingress from the API security group to the private interface endpoints"
+  type                     = "ingress"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.vpc_endpoint.id
+  source_security_group_id = aws_security_group.app.id
+}
+
+resource "aws_security_group_rule" "vpc_endpoint_dynamodb_ingress" {
+  description       = "Ingress from the private DynamoDB endpoint"
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  security_group_id = aws_security_group.vpc_endpoint.id
+  prefix_list_ids = [
+    aws_vpc_endpoint.rds.prefix_list_id
+  ]
+}
+
+resource "aws_security_group_rule" "s3_private_endpoint_ingress" {
+  description       = "Ingress from the private S3 endpoint"
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  security_group_id = aws_security_group.vpc_endpoint.id
+  prefix_list_ids = [
+    aws_vpc_endpoint.s3.prefix_list_id
+  ]
+}
+
+resource "aws_flow_log" "cloud_based_sensor" {
+  log_destination      = "arn:aws:s3:::${var.cbs_satellite_bucket_name}/vpc_flow_logs/"
+  log_destination_type = "s3"
+  traffic_type         = "ALL"
+  vpc_id               = module.saas_procurement_vpc.vpc_id
+  log_format           = "$${vpc-id} $${version} $${account-id} $${interface-id} $${srcaddr} $${dstaddr} $${srcport} $${dstport} $${protocol} $${packets} $${bytes} $${start} $${end} $${action} $${log-status} $${subnet-id} $${instance-id}"
+
+  tags = {
+    CostCentre = var.billing_code
+    Terraform  = true
+  }
+}
