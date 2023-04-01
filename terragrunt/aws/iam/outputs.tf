e08fb5e0da863cc2bdedc6ae2d175c4594566b22
Sylvia McLaughlin <sylvia.mclaughlin@cds-snc.ca>
2023-03-31 20:26:08 -0700

    Changing casing of description

--- terragrunt/aws/iam/outputs.tf
+++ terragrunt/aws/iam/outputs.tf
@@ -1,10 +1,10 @@
 output "iam_role_saas_procurement_arn" {
-  description = "ARN of the Saas procurement role"
+  description = "Arn of the Saas procurement role"
   value       = aws_iam_role.saas_procurement.arn
 }
 
 output "iam_role_saas_procurement_task_arn" {
-  description = "ARN of the Saas procurement task role"
+  description = "Arn of the Saas procurement task role"
   value	      = aws_iam_role.saas_procurement_task.arn
 }
 
