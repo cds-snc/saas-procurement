1baece740e4a06a24cccc6515e78b3104771207f
Sylvia McLaughlin <85905333+sylviamclaughlin@users.noreply.github.com>
2023-04-06 15:01:29 -0700

    Fixing up IAM policies (#81)
    * Fixing up the iam policies
    
    * updating iam policies and creating a plicy document and attaching it to a role
    
    * Fixing up formatting

--- terragrunt/aws/iam/iam.tf
+++ terragrunt/aws/iam/iam.tf
@@ -7,7 +7,9 @@ data "aws_iam_policy_document" "saas_procurement" {
       identifiers = ["ecs-tasks.amazonaws.com"]
     }
   }
+}
 
+data "aws_iam_policy_document" "saas_procurement_ssm" {
   statement {
     effect = "Allow"
     actions = [
@@ -16,6 +18,17 @@ data "aws_iam_policy_document" "saas_procurement" {
     resources = [
       var.postgres_password_arn
     ]
+  }
+}
+
+resource "aws_iam_policy" "saas_procurement_ssm" {
+  name        = "${var.product_name}_Ssm"
+  description = "Allow retrieval of ssm data"
+  policy      = data.aws_iam_policy_document.saas_procurement_ssm.json
+
+  tags = {
+    CostCentre = var.billing_code
+    Terraform  = true
   }
 }
 
@@ -43,3 +56,9 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution" {
   role       = aws_iam_role.saas_procurement.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
+
+resource "aws_iam_role_policy_attachment" "saas_procurement_ssm" {
+  policy_arn = aws_iam_policy.saas_procurement_ssm.arn
+  role       = aws_iam_role.saas_procurement_task.name
+}
+
