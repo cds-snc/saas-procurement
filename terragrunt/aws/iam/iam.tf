d50807b94bd1a1e30c39d5732cff967811a6996d
Sylvia McLaughlin <sylvia.mclaughlin@cds-snc.ca>
2023-04-06 11:44:14 -0700

    updating iam policies and creating a plicy document and attaching it to a role

--- terragrunt/aws/iam/iam.tf
+++ terragrunt/aws/iam/iam.tf
@@ -21,6 +21,17 @@ data "aws_iam_policy_document" "saas_procurement_ssm" {
   }
 }
 
+resource "aws_iam_policy" "saas_procurement_ssm" {
+  name        = "${var.product_name}_Ssm"
+  description = "Allow retrieval of ssm data"
+  policy      = data.aws_iam_policy_document.saas_procurement_ssm.json
+
+  tags = {
+    CostCentre = var.billing_code
+    Terraform  = true
+  }
+}
+
 resource "aws_iam_role" "saas_procurement" {
   name = "saas_procurement-ecs-role"
 
@@ -45,3 +56,9 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution" {
   role       = aws_iam_role.saas_procurement.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
+
+resource "aws_iam_role_policy_attachment" "saas_procurement_ssm" {
+  policy_arn = aws_iam_policy.saas_procurement_ssm.arn
+  role       = aws_iam_role.saas_procurement_task.name 
+}
+
