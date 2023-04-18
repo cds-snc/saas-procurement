d2b64b24f0b646f1ee5c05c7c5e0c0c00b8345ee
Sylvia McLaughlin <85905333+sylviamclaughlin@users.noreply.github.com>
2023-04-18 13:43:53 -0700

    Adding files to make app work on AWS (#86)
    * Adding files to make app work on AWS
    
    * Fixing formatting
    
    * Fixing up tabbing
    
    * Removing unecesary package
    
    * Changing the site id
    
    * Fixing linting
    
    * Adding changes to make the app work on aws
    
    * Formatting files
    
    * Removing the * in ALLOWED_HOSTS to make it more secure
    
    * Adding library needed for gethostname
    
    * changing the service name

---
+++ saas_app/bin/get_parameters.py
@@ -0,0 +1,10 @@
+import boto3
+
+# Generate .env file from AWS parameter store
+ssm = boto3.client("ssm", region_name="ca-central-1")
+saas_app_config = ssm.get_parameter(Name="saas_app_config", WithDecryption=True)[
+    "Parameter"
+]["Value"]
+env_file = open(".env", "w")
+env_file.write(saas_app_config)
+env_file.close()
