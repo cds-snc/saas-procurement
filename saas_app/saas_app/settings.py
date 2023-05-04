d6a20b2cac45ee28d292be985b30b1638d2d8179
Sylvia McLaughlin <sylvia.mclaughlin@cds-snc.ca>
2023-05-04 12:57:33 -0700

    Changing settings file

--- saas_app/saas_app/settings.py
+++ saas_app/saas_app/settings.py
@@ -192,7 +192,7 @@ if "test" in sys.argv:
 if os.environ.get("ENVIRONMENT") == "dev":
     SITE_ID = 9 
 else:
-    SITE_ID = 12 
+    SITE_ID = 13 
     ACCOUNT_DEFAULT_HTTP_PROTOCOL = "https"
 
 
@@ -210,4 +210,4 @@ CRISPY_TEMPLATE_PACK = "bootstrap4"
 TESTING_FEATURE_FLAG = os.environ.get("TESTING_FEATURE_FLAG")
 
 # Set up the session engine
-SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+SESSION_ENGINE = "django.contrib.sessions.backends.db"
