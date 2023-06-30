fa3d65ced6eacf853ded93486f137437adecff97
Sylvia McLaughlin <85905333+sylviamclaughlin@users.noreply.github.com>
2023-06-30 23:17:33 +0000

    Modifying iam configuration to take account the new ssm parameters

--- terragrunt/aws/iam/iam.tf
+++ terragrunt/aws/iam/iam.tf
@@ -19,7 +19,36 @@ data "aws_iam_policy_document" "saas_procurement_ssm" {
       "ssm:GetParameters",
       "ssm:GetParameter"
     ]
-    resources = ["arn:aws:ssm:*:*:parameter/*"]
+    resources = [
+      aws_ssm_parameter.saas_app_config.arn,
+      aws_ssm_parameter.approved_request_template_id,
+      aws_ssm_parameter.request_s32_approved_template_id,
+      aws_ssm_parameter.postgres_password,
+      aws_ssm_parameter.environment,
+      aws_ssm_parameter.django_secret_key,
+      aws_ssm_parameter.approver_delete_template_id,
+      aws_ssm_parameter.social_application_client_id,
+      aws_ssm_parameter.saas_submission_template_id,
+      aws_ssm_parameter.edit_request_template_id,
+      aws_ssm_parameter.delete_saas_request_template_id,
+      aws_ssm_parameter.saas_submission_edit_template_id,
+      aws_ssm_parameter.denied_request_template_id,
+      aws_ssm_parameter.notify_url,
+      aws_ssm_parameter.notify_api_key,
+      aws_ssm_parameter.approval_request_template_id,
+      aws_ssm_parameter.request_s32_denied_internal_ops_template_id,
+      aws_ssm_parameter.db_host,
+      aws_ssm_parameter.internal_ops_request_more_info_template_id,
+      aws_ssm_parameter.postgres_user,
+      aws_ssm_parameter.request_s32_denied_template_id,
+      aws_ssm_parameter.s32_approval_requested_template_id,
+      aws_ssm_parameter.postgres_db,
+      aws_ssm_parameter.request_s32_approved_internal_ops_template_id,
+      aws_ssm_parameter.requestor_s32approval_pending_review_template_id,
+      aws_ssm_parameter.social_application_secret_key,
+      aws_ssm_parameter.testing_feature_flag,
+      aws_ssm_parameter.site_id
+    ]
   }
 }
 
--- terragrunt/env/staging/iam/terragrunt.hcl
+++ terragrunt/env/staging/iam/terragrunt.hcl
@@ -12,7 +12,34 @@ dependency "ssm" {
   mock_outputs_allowed_terraform_commands = ["init", "fmt", "validate", "plan", "show"]
   mock_outputs_merge_with_state           = true
   mock_outputs = {
-    saas_app_config_arn = ""
+    saas_app_config_arn = "",
+    approved_request_template_id = "",
+    request_s32_approved_template_id = "",
+    postgres_password = "",
+    environment = "",
+    django_secret_key = "",
+    approver_delete_template_id = "",
+    social_application_client_id = "",
+    saas_submission_template_id = "",
+    edit_request_template_id = "", 
+    delete_saas_request_template_id = "",
+    saas_submission_edit_template_id = "",
+    denied_request_template_id = "",
+    notify_url = "",
+    notify_api_key = "",
+    approval_request_template_id = "",
+    request_s32_denied_internal_ops_template_id = "",
+    db_host = "",
+    internal_ops_request_more_info_template_id = "",
+    postgres_user = "",
+    request_s32_denied_template_id = "",
+    s32_approval_requested_template_id = "",
+    postgres_db = "",
+    request_s32_approved_internal_ops_template_id = "",
+    requestor_s32approval_pending_review_template_id = "",
+    social_application_secret_key = "",
+    testing_feature_flag = "",
+    site_id = "", 
   }
 }
 
