4fa530bf365a759c548d8348d56ce341b4e5abf3
Sylvia McLaughlin <85905333+sylviamclaughlin@users.noreply.github.com>
2023-07-14 14:42:08 -0700

    Google workspace logs integration (#193)
    * Adding logic to query the log the sentinel logs to display the information
    
    * Adding updated requirements.txt file
    
    * Adding pandas
    
    * Adding an extra a at the end of the description
    
    * Adding updated requirements and configured the sentinel app proprly to connect
    
    * Adding no text wrapping to display data better
    
    * Linting code

--- saas_app/administration/views.py
+++ saas_app/administration/views.py
@@ -3,6 +3,69 @@ from django.utils.translation import gettext as _
 from .forms import ViewUserForm
 from user.models import Users, Roles
 import django.contrib.messages as messages
+from azure.identity import DefaultAzureCredential
+from azure.monitor.query import LogsQueryClient, LogsQueryStatus
+from datetime import datetime, timezone, timedelta
+import os
+import pandas as pd
+
+
+# function to get the azure credentials and get a client to query the logs. Return the client to search for logs.
+def azure_get_credentials():
+    # get the client id
+    client_id = os.environ["AZURE_CLIENT_ID"]
+    credentials = DefaultAzureCredential(managed_identity_client_id=client_id)
+    logs_client = LogsQueryClient(credentials)
+    return logs_client
+
+
+# function to get the logs from Sentinel. Return the logs in a dataframe to display in the template as a table
+def get_logs(request):
+    # log into azure and get the logs client
+    logs_client = azure_get_credentials()
+
+    # query to get the logs from Sentinel, specifically the GWorkspace_ReportsAPI_token_CL table for the last 7 days
+    query = """GWorkspace_ReportsAPI_token_CL"""
+    start_date = datetime.now(tz=timezone.utc) - timedelta(days=7)
+    end_date = datetime.now(tz=timezone.utc)
+
+    try:
+        # isssue the query to get the logs
+        response = logs_client.query_workspace(
+            workspace_id=os.environ["LOG_ANALYTICS_WORKSPACE_ID"],
+            query=query,
+            timespan=(start_date, end_date),
+        )
+        if response.status == LogsQueryStatus.PARTIAL:
+            error = response.partial_error
+            data = response.partial_data
+            # generate an error if the query was not successful
+            messages.error(
+                request,
+                _(
+                    "There was an error retrieving the logs! The query did not execute fully and the results may be incomplete"
+                ),
+            )
+            print(error)
+        elif response.status == LogsQueryStatus.SUCCESS:
+            data = response.tables
+            # represent the response in a pandas dataframe
+            for table in data:
+                df = pd.DataFrame(data=table.rows, columns=table.columns)
+            return df
+    except Exception as err:
+        print("An error was encountered: ", err)
+        messages.error(
+            request,
+            _("There was an error retrieving the logs! The query was not successful!"),
+        )
+
+
+# function to view all the logs from Sentinel
+def view_logs(request):
+    if request.method == "GET":
+        logs = get_logs(request)
+        return render(request, "administration/view_all_logs.html", {"all_logs": logs})
 
 
 # function to return the list of users. If POST request, then we update the user data
@@ -25,6 +88,7 @@ def view_users(request):
 # function to view a single user and update the user data
 def view_user(request, pk):
     if request.method == "GET":
+        get_logs(request)
         # search for the user with the given primary key
         user = Users.objects.get(pk=pk)
         form = ViewUserForm(instance=user)
