a72141cc2df03893d6f13a82799d7c3861983b9c
Sylvia McLaughlin <85905333+sylviamclaughlin@users.noreply.github.com>
2023-07-14 20:12:28 +0000

    Linting code

--- saas_app/administration/views.py
+++ saas_app/administration/views.py
@@ -12,7 +12,7 @@ import pandas as pd
 
 # function to get the azure credentials and get a client to query the logs. Return the client to search for logs.
 def azure_get_credentials():
-    # get the client id 
+    # get the client id
     client_id = os.environ["AZURE_CLIENT_ID"]
     credentials = DefaultAzureCredential(managed_identity_client_id=client_id)
     logs_client = LogsQueryClient(credentials)
@@ -21,7 +21,6 @@ def azure_get_credentials():
 
 # function to get the logs from Sentinel. Return the logs in a dataframe to display in the template as a table
 def get_logs(request):
-    
     # log into azure and get the logs client
     logs_client = azure_get_credentials()
 
