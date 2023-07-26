77613e976b35de1af9d617525c8372896d1b5d4b
Sylvia McLaughlin <85905333+sylviamclaughlin@users.noreply.github.com>
2023-07-26 17:11:14 +0000

    Adding code for the version to work in production as well

--- saas_app/saas_app/templatetags/version.py
+++ saas_app/saas_app/templatetags/version.py
@@ -1,6 +1,7 @@
 from django import template
 import subprocess
 import datetime
+import os
 
 register = template.Library()
 
@@ -8,19 +9,29 @@ register = template.Library()
 # A custom template tag to display when the application was last updated and the git sha.
 @register.simple_tag
 def version():
+    # set the commit and time to unknown
+    commit = time = "Unknown"
+    print(os.getenv("GIT_SHA"))
+
     # get commit sha
-    commit = (
-        subprocess.check_output(["git", "rev-parse", "HEAD"]).decode("utf-8").strip()
-    )
+    if (os.getenv("ENVIRONMENT") == "dev"):
+         # the the commit 
+        commit = (
+            subprocess.check_output(["git", "rev-parse", "HEAD"]).decode("utf-8").strip()
+        )
 
-    # get the time and format it locally
-    time = (
-        subprocess.check_output(["git", "log", "-1", "--format=%cd"])
-        .decode("utf-8")
-        .strip()
-    )
-    parsed_time = datetime.datetime.strptime(time, "%a %b %d %H:%M:%S %Y %z")
-    formatted_time = parsed_time.strftime("%a %b %d %H:%M:%S %Y")
+        # get the time and format it locally
+        date_time = (
+            subprocess.check_output(["git", "log", "-1", "--format=%cd"])
+            .decode("utf-8")
+            .strip()
+        )
+        parsed_date_time = datetime.datetime.strptime(date_time, "%a %b %d %H:%M:%S %Y %z")
+        formatted_date_time = parsed_date_time.strftime("%a %b %d %H:%M:%S %Y")
+    else:
 
+        commit = os.getenv("GIT_SHA")
+        formatted_date_time = os.getenv("BUILD_DATE")
+    
     # return the formatted time and git sha
-    return f"{formatted_time}, Git SHA: {commit}"
+    return f"{formatted_date_time}, Git SHA: {commit}"
