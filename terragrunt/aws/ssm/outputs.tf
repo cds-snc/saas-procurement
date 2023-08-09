1649d15d8353439a190ec7246ce2ab077ad1cf72
Sylvia McLaughlin <85905333+sylviamclaughlin@users.noreply.github.com>
2023-08-09 21:18:18 +0000

    Adding files to scale down saas app

--- terragrunt/aws/ssm/outputs.tf
+++ terragrunt/aws/ssm/outputs.tf
@@ -2,9 +2,3 @@ output "saas_app_config_arn" {
   description = "Arn of the saas app config parameter"
   value       = aws_ssm_parameter.saas_app_config.arn
 }
-
-output "saas_app_config_value" {
-  description = "The value of the saas app config parameter"
-  value       = aws_ssm_parameter.saas_app_config.value
-  sensitive   = true
-}
