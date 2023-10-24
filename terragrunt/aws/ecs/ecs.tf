f9c037f9c285b4c3f95795c6df0ddc98179c6ad1
Sylvia McLaughlin <85905333+sylviamclaughlin@users.noreply.github.com>
2023-10-24 00:00:48 +0000

    Adding changes to use ecs module

--- terragrunt/aws/ecs/ecs.tf
+++ terragrunt/aws/ecs/ecs.tf
@@ -1,107 +1,41 @@
-# Cluster
-resource "aws_ecs_cluster" "saas_procurement" {
-  name = "saas-procurement-cluster"
+module "saas_procurement" {
+  source = "github.com/cds-snc/terraform-modules//ecs?ref=v7.2.7"
 
-  setting {
-    name  = "containerInsights"
-    value = "enabled"
-  }
-}
-
-# Task definition
-data "template_file" "saas_procurement" {
-  template = file("./templates/saas_procurement.json.tpl")
-
-  vars = {
-    awslogs-group         = aws_cloudwatch_log_group.saas_procurement_group.name
-    awslogs-region        = "ca-central-1"
-    awslogs-stream-prefix = "ecs-saas_procurement"
-    image                 = "${var.ecr_repository_url}:latest"
-    fargate_cpu           = var.fargate_cpu
-    fargate_memory        = var.fargate_memory
-    aws_region            = "ca-central-1"
-  }
-}
-
-resource "aws_ecs_task_definition" "saas_procurement" {
-  family                   = "saas-procurement-task"
-  execution_role_arn       = var.iam_role_saas_procurement_task_arn
-  network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-  cpu                      = var.fargate_cpu
-  memory                   = var.fargate_memory
-  container_definitions    = data.template_file.saas_procurement.rendered
-  task_role_arn            = var.iam_role_saas_procurement_task_arn
-
-  runtime_platform {
-    operating_system_family = "LINUX"
-    cpu_architecture        = "X86_64"
-  }
-}
-
-# Service
-resource "aws_ecs_service" "saas-procurement-app-service" {
-  name             = "saas_procurement-service"
-  cluster          = aws_ecs_cluster.saas_procurement.id
-  task_definition  = aws_ecs_task_definition.saas_procurement.arn
-  desired_count    = 1
-  launch_type      = "FARGATE"
-  platform_version = "1.4.0"
-  propagate_tags   = "SERVICE"
-
-  network_configuration {
-    security_groups  = [aws_security_group.ecs_tasks.id]
-    subnets          = var.vpc_private_subnet_ids
-    assign_public_ip = false
-  }
-
+  # Cluster and service
+  cluster_name = "saas-procurement-cluster"
+  service_name = "saas-procurement-app-service"
   depends_on = [
     var.lb_listener,
     var.ecs_task_policy_attachment
   ]
 
-  load_balancer {
-    target_group_arn = var.lb_target_group_arn
-    container_name   = "saas-procurement"
-    container_port   = 8000
-  }
+  # Task/Container definition
+  container_image     = "${var.ecr_repository_url}:latest"
+  container_name      = "saas-procurement"
+  task_cpu            = var.fargate_cpu
+  task_memory         = var.fargate_memory
+  container_port      = 8000
+  container_host_port = 8000
 
-  tags = {
-    "CostCentre" = var.billing_code
-  }
+  # Task definition
+  task_name          = "saas-procurement-task"
+  task_exec_role_arn = var.iam_role_saas_procurement_task_arn
+  task_role_arn      = var.iam_role_saas_procurement_task_arn
+
+
+  # Scaling
+  enable_autoscaling = true
+
+  # Networking
+  lb_target_group_arn = var.lb_target_group_arn
+  security_group_ids  = [aws_security_group.ecs_tasks.id]
+  subnet_ids          = var.vpc_private_subnet_ids
+
+  billing_tag_value = var.billing_code
 }
 
-resource "aws_cloudwatch_log_group" "saas_procurement_group" {
-  name              = "/ecs/saas-procurement-app"
-  retention_in_days = 30
-
-  tags = {
-    Name = "saas-procurement-log-group"
-  }
-}
 
 resource "aws_cloudwatch_log_stream" "saas_procurement_stream" {
   name           = "saas-procurement-log-stream"
-  log_group_name = aws_cloudwatch_log_group.saas_procurement_group.name
-}
-
-module "sentinel_forwarder" {
-  source            = "github.com/cds-snc/terraform-modules//sentinel_forwarder?ref=v6.1.5"
-  function_name     = "sentinel-forwarder"
-  billing_tag_value = var.billing_tag_value
-
-  layer_arn = "arn:aws:lambda:ca-central-1:283582579564:layer:aws-sentinel-connector-layer:71"
-
-  customer_id = var.sentinel_customer_id
-  shared_key  = var.sentinel_shared_key
-
-  cloudwatch_log_arns = [aws_cloudwatch_log_group.saas_procurement_group.arn]
-}
-
-resource "aws_cloudwatch_log_subscription_filter" "sentinel_forwarder" {
-  name            = "All ECS logs"
-  log_group_name  = aws_cloudwatch_log_group.saas_procurement_group.name
-  filter_pattern  = "[w1=\"*\"]"
-  destination_arn = module.sentinel_forwarder.lambda_arn
-  distribution    = "Random"
+  log_group_name = "/aws/ecs/saas-procurement-cluster"
 }
