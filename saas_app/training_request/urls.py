e9c4a6e0c76ee585e4cc4d35deb7f8199ec27120
Sylvia McLaughlin <85905333+sylviamclaughlin@users.noreply.github.com>
2023-12-13 15:14:44 -0800

    Adding option to download completed form from the submitted requests (#305)

--- saas_app/training_request/urls.py
+++ saas_app/training_request/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 app_name = "training_request"
@@ -9,3 +11,4 @@ urlpatterns = [
     path("view/<int:pk>/", views.view_request, name="view_request"),
     path("download/", views.download, name="download"),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
