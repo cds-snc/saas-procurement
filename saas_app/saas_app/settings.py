0cfbd7f13f0ca294ea03901004b6ddedb2a77aea
Sylvia McLaughlin <85905333+sylviamclaughlin@users.noreply.github.com>
2024-07-12 19:06:59 +0000

    Adding a comment to the middleware addition

--- saas_app/saas_app/settings.py
+++ saas_app/saas_app/settings.py
@@ -80,7 +80,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "saas_app.middleware.HealthCheckMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
-    "allauth.account.middleware.AccountMiddleware",
+    "allauth.account.middleware.AccountMiddleware",  # add the allauth middleware
 ]
 
 ROOT_URLCONF = "saas_app.urls"
